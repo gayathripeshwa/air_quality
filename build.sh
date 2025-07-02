ccd79b4f0c284d72599526ed7f64eae4065a6c1b
Gayathri Gopal Peshwa <gayathripeshwa18@gmail.com>
2025-07-02 12:09:11 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,11 @@ PYTHON_VERSION=3.11.8
 pyenv install -s $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
 
-# Install dependencies globally for this Python version
+# Create virtual environment and install dependencies
+python -m venv venv
+source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# Make streamlit globally accessible at runtime
+ln -s $(pwd)/venv/bin/streamlit /usr/local/bin/streamlit
